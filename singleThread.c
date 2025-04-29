1db52d9ffffac8d5bb852ff84845f59bddc14230
ysbbin <ysb5391@gmail.com>
2025-04-29 10:47:47 +0900

    Update singleThread.c

--- singleThread.c
+++ singleThread.c
@@ -26,6 +26,43 @@ void print_resource_usage() {
     printf("Major Page Faults: %ld\n", usage.ru_majflt);
 }
 
+// CNN forward를 수행할 스레드 함수
+void* cnn_forward_thread(void* arg) {
+    Conv2DLayer* conv_layer = ((Conv2DLayer**)arg)[0];
+    MaxPool2DLayer* pool_layer = ((MaxPool2DLayer**)arg)[1];
+    FullyConnected1Layer* fc1_layer = ((FullyConnected1Layer**)arg)[2];
+    FullyConnected2Layer* fc2_layer = ((FullyConnected2Layer**)arg)[3];
+
+    double (*conv_output)[VALID_SIZE][VALID_SIZE] = malloc(sizeof(double) * 16 * VALID_SIZE * VALID_SIZE);
+    double (*pool_output)[POOL_SIZE][POOL_SIZE] = malloc(sizeof(double) * 16 * POOL_SIZE * POOL_SIZE);
+    double* flatten_output = (double*)malloc(sizeof(double) * FC1_INPUT_SIZE);
+    double* fc1_output = (double*)malloc(sizeof(double) * FC1_SIZE);
+    double* fc2_output = (double*)malloc(sizeof(double) * FC2_SIZE);
+
+    for (int idx = 0; idx < NUM_INPUTS; idx++) {
+        printf("\n[Input #%d]\n", idx + 1);
+
+        conv2d_forward(conv_layer, input_streams[idx], conv_output);
+        relu_forward(conv_output);
+        maxpool2d_forward(pool_layer, conv_output, pool_output);
+        flatten_forward(pool_output, flatten_output);
+        fc1_forward(fc1_layer, flatten_output, fc1_output);
+        fc2_forward(fc2_layer, fc1_output, fc2_output);
+
+        printf("FC2 output sample: ");
+        for (int i = 0; i < 5; i++) printf("%.5f ", fc2_output[i]);
+        printf("\n");
+    }
+
+    free(flatten_output);
+    free(fc1_output);
+    free(fc2_output);
+    free(conv_output);
+    free(pool_output);
+
+    pthread_exit(NULL);
+}
+
 int main() {
     init_input_streams();
 
@@ -76,44 +113,17 @@ int main() {
     for (int j = 0; j < FC2_SIZE; j++)
         fc2_layer.bias[j] = (j % 2 == 0) ? 0.5 : 0.0;
 
-    // 출력 메모리 할당
-    double (*conv_output)[VALID_SIZE][VALID_SIZE] = malloc(sizeof(double) * 16 * VALID_SIZE * VALID_SIZE);
-    double (*pool_output)[POOL_SIZE][POOL_SIZE] = malloc(sizeof(double) * 16 * POOL_SIZE * POOL_SIZE);
-    double* flatten_output = (double*)malloc(sizeof(double) * FC1_INPUT_SIZE);
-    double* fc1_output = (double*)malloc(sizeof(double) * FC1_SIZE);
-    double* fc2_output = (double*)malloc(sizeof(double) * FC2_SIZE);
+    // 스레드 생성
+    pthread_t tid;
+    void* args[4] = { &conv_layer, &pool_layer, &fc1_layer, &fc2_layer };
+    pthread_create(&tid, NULL, cnn_forward_thread, args);
 
-    if (!conv_output || !pool_output || !flatten_output || !fc1_output || !fc2_output) {
-        printf("Memory allocation failed!\n");
-        return -1;
-    }
-
-    printf("\n===== Start processing all input streams in single thread =====\n");
-
-    for (int idx = 0; idx < NUM_INPUTS; idx++) {
-        printf("\n[Input #%d]\n", idx + 1);
-
-        conv2d_forward(&conv_layer, input_streams[idx], conv_output);
-        relu_forward(conv_output);
-        maxpool2d_forward(&pool_layer, conv_output, pool_output);
-        flatten_forward(pool_output, flatten_output);
-        fc1_forward(&fc1_layer, flatten_output, fc1_output);
-        fc2_forward(&fc2_layer, fc1_output, fc2_output);
-
-        printf("FC2 output sample: ");
-        for (int i = 0; i < 5; i++) printf("%.5f ", fc2_output[i]);
-        printf("\n");
-    }
+    // 스레드 끝날 때까지 대기
+    pthread_join(tid, NULL);
 
     print_resource_usage();
 
     // 메모리 해제
-    free(flatten_output);
-    free(fc1_output);
-    free(fc2_output);
-    free(conv_output);
-    free(pool_output);
-
     for (int f = 0; f < conv_layer.out_channels; f++) {
         for (int c = 0; c < conv_layer.in_channels; c++) {
             for (int i = 0; i < 3; i++) {
