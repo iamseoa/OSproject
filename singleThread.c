07489734d437202f05e430ac6ed41d96e359e75d
ysbbin <ysb5391@gmail.com>
2025-05-03 18:25:17 +0900

    Update singleThread.c

--- singleThread.c
+++ singleThread.c
@@ -1,6 +1,7 @@
 #include "layers.h"
 #include <sys/resource.h>
 #include <pthread.h>
+#include <unistd.h> // for getpid()
 
 #define NUM_INPUTS 9
 
@@ -26,7 +27,6 @@ void print_resource_usage() {
     printf("Major Page Faults: %ld\n", usage.ru_majflt);
 }
 
-// CNN forward를 수행할 스레드 함수
 void* cnn_forward_thread(void* arg) {
     Conv2DLayer* conv_layer = ((Conv2DLayer**)arg)[0];
     MaxPool2DLayer* pool_layer = ((MaxPool2DLayer**)arg)[1];
@@ -39,8 +39,10 @@ void* cnn_forward_thread(void* arg) {
     double* fc1_output = (double*)malloc(sizeof(double) * FC1_SIZE);
     double* fc2_output = (double*)malloc(sizeof(double) * FC2_SIZE);
 
+    pid_t pid = getpid();
+
     for (int idx = 0; idx < NUM_INPUTS; idx++) {
-        printf("\n[Input #%d]\n", idx + 1);
+        printf("\n===== [PID %d] Finished input stream #%d =====\n", pid, idx + 1);
 
         conv2d_forward(conv_layer, input_streams[idx], conv_output);
         relu_forward(conv_output);
@@ -48,6 +50,14 @@ void* cnn_forward_thread(void* arg) {
         flatten_forward(pool_output, flatten_output);
         fc1_forward(fc1_layer, flatten_output, fc1_output);
         fc2_forward(fc2_layer, fc1_output, fc2_output);
+
+        printf("Conv2D output sample: ");
+        for (int i = 0; i < 5; i++) printf("%.5f ", conv_output[0][i][i]);
+        printf("\n");
+
+        printf("FC1 output sample: ");
+        for (int i = 0; i < 5; i++) printf("%.5f ", fc1_output[i]);
+        printf("\n");
 
         printf("FC2 output sample: ");
         for (int i = 0; i < 5; i++) printf("%.5f ", fc2_output[i]);
@@ -66,7 +76,6 @@ void* cnn_forward_thread(void* arg) {
 int main() {
     init_input_streams();
 
-    // Layer 초기화
     Conv2DLayer conv_layer;
     conv_layer.in_channels = 3;
     conv_layer.out_channels = 16;
@@ -113,17 +122,13 @@ int main() {
     for (int j = 0; j < FC2_SIZE; j++)
         fc2_layer.bias[j] = (j % 2 == 0) ? 0.5 : 0.0;
 
-    // 스레드 생성
     pthread_t tid;
     void* args[4] = { &conv_layer, &pool_layer, &fc1_layer, &fc2_layer };
     pthread_create(&tid, NULL, cnn_forward_thread, args);
-
-    // 스레드 끝날 때까지 대기
     pthread_join(tid, NULL);
 
     print_resource_usage();
 
-    // 메모리 해제
     for (int f = 0; f < conv_layer.out_channels; f++) {
         for (int c = 0; c < conv_layer.in_channels; c++) {
             for (int i = 0; i < 3; i++) {
