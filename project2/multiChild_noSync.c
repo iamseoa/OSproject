852f9d8e3e6f134b399421102ae2c6cb2a5a6e2f
iamseoa <iamseoalee@gmail.com>
2025-05-12 16:00:15 +0900

    add optimization version

---
+++ project2/multiChild_noSync.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <sys/syscall.h>
+#include "cnn_common.h"
+
+#define gettid() syscall(SYS_gettid)
+
+CNNModel *model;
+float (*inputs)[INPUT_SIZE][INPUT_SIZE][CHANNELS];
+
+int main() {
+    model = mmap(NULL, sizeof(CNNModel), PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+    inputs = mmap(NULL, sizeof(float) * NUM_INPUTS * INPUT_SIZE * INPUT_SIZE * CHANNELS, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+    initialize_weights(model);
+    for (int i = 0; i < NUM_INPUTS; i++) {
+        initialize_input(inputs[i], i);
+    }
+
+    struct timeval start, end;
+    gettimeofday(&start, NULL);
+
+    for (int i = 0; i < NUM_INPUTS; i++) {
+        pid_t pid = fork();
+        if (pid == 0) {
+            printf("\n== Child PID %d, TID %ld processing input %d ==\n", getpid(), (long)gettid(), i);
+
+            conv_forward(model, inputs[i], model->conv_output);
+            relu_forward(model->conv_output, model->relu_output);
+            maxpool_forward(model->relu_output, model->pooled_output);
+
+            float flat[(CONV_OUT / 2) * (CONV_OUT / 2) * CONV_DEPTH];
+            flatten(model->pooled_output, flat);
+            fc1_forward(model, flat, model->fc1_output);
+            fc2_forward(model, model->fc1_output, model->fc2_output);
+
+            printf("Input Patch [0:3][0:3][0]:\n");
+            for (int x = 0; x < 3; x++) {
+                for (int y = 0; y < 3; y++) {
+                    printf("%.1f ", inputs[i][x][y][0]);
+                }
+                printf("\n");
+            }
+
+            printf("Conv Output [0:5][0][0] = ");
+            for (int j = 0; j < 5; j++) printf("%.2f ", model->conv_output[j][0][0]);
+            printf("\nfc1[0:5] = ");
+            for (int j = 0; j < 5; j++) printf("%.2f ", model->fc1_output[j]);
+            printf("\nfc2[0:5] = ");
+            for (int j = 0; j < 5; j++) printf("%.2f ", model->fc2_output[j]);
+            printf("\n");
+
+            exit(0);
+        }
+    }
+
+    for (int i = 0; i < NUM_INPUTS; i++) wait(NULL);
+
+    gettimeofday(&end, NULL);
+    struct rusage usage;
+    getrusage(RUSAGE_SELF, &usage);
+    double elapsed = (end.tv_sec - start.tv_sec) * 1000.0 + (end.tv_usec - start.tv_usec) / 1000.0;
+
+    printf("\n== Performance Metrics ==\n");
+    printf("Elapsed time       : %.3f ms\n", elapsed);
+    printf("Max RSS memory     : %ld KB\n", usage.ru_maxrss);
+    printf("Voluntary context switches   : %ld\n", usage.ru_nvcsw);
+    printf("Involuntary context switches : %ld\n", usage.ru_nivcsw);
+
+    return 0;
+}
--- project2/multiThread_pipeline.c
+++ project2/multiThread_pipeline.c
@@ -1,130 +1,38 @@
+#include "cnn_common.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <unistd.h>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <unistd.h>
 #include <sys/syscall.h>
 #define gettid() syscall(SYS_gettid)
-
-#define INPUT_SIZE 64
-#define CHANNELS 3
-#define KERNEL_SIZE 3
-#define CONV_OUT (INPUT_SIZE - KERNEL_SIZE + 1)
-#define CONV_DEPTH 16
-#define FC1_OUT 256
-#define FC2_OUT 100
-#define NUM_INPUTS 9
 
 typedef struct {
     float input[INPUT_SIZE][INPUT_SIZE][CHANNELS];
     float conv_output[CONV_OUT][CONV_OUT][CONV_DEPTH];
     float relu_output[CONV_OUT][CONV_OUT][CONV_DEPTH];
-    float pooled_output[CONV_OUT/2][CONV_OUT/2][CONV_DEPTH];
-    float flat[(CONV_OUT/2)*(CONV_OUT/2)*CONV_DEPTH];
+    float pooled_output[CONV_OUT / 2][CONV_OUT / 2][CONV_DEPTH];
+    float flat[(CONV_OUT / 2) * (CONV_OUT / 2) * CONV_DEPTH];
     float fc1_output[FC1_OUT];
     float fc2_output[FC2_OUT];
     int id;
 } Sample;
 
-typedef struct {
-    float weights[CONV_DEPTH][CHANNELS][KERNEL_SIZE][KERNEL_SIZE];
-    float biases[CONV_DEPTH];
-} ConvLayer;
-
-typedef struct {
-    float weights[FC1_OUT][(CONV_OUT/2)*(CONV_OUT/2)*CONV_DEPTH];
-    float biases[FC1_OUT];
-} FullyConnectedLayer1;
-
-typedef struct {
-    float weights[FC2_OUT][FC1_OUT];
-    float biases[FC2_OUT];
-} FullyConnectedLayer2;
-
-typedef struct {
-    ConvLayer conv;
-    FullyConnectedLayer1 fc1;
-    FullyConnectedLayer2 fc2;
-} CNNModel;
-
 CNNModel model;
 Sample samples[NUM_INPUTS];
 pthread_mutex_t print_mutex;
-
-void initialize_input(float input[INPUT_SIZE][INPUT_SIZE][CHANNELS], int id) {
-    for (int c = 0; c < CHANNELS; c++)
-        for (int i = 0; i < INPUT_SIZE; i++)
-            for (int j = 0; j < INPUT_SIZE; j++)
-                input[i][j][c] = (i == 1 && j == 1) ? (float)(9 * (id + 1)) : 1.0f;
-}
-
-void initialize_weights() {
-    int kernel[3][3] = {{1,2,1},{2,4,2},{1,2,1}};
-    for (int d = 0; d < CONV_DEPTH; d++) {
-        model.conv.biases[d] = 1.0f;
-        for (int c = 0; c < CHANNELS; c++)
-            for (int i = 0; i < KERNEL_SIZE; i++)
-                for (int j = 0; j < KERNEL_SIZE; j++)
-                    model.conv.weights[d][c][i][j] = (float)kernel[i][j];
-    }
-    for (int i = 0; i < FC1_OUT; i++) {
-        model.fc1.biases[i] = 1.0f;
-        for (int j = 0; j < (CONV_OUT/2)*(CONV_OUT/2)*CONV_DEPTH; j++)
-            model.fc1.weights[i][j] = (i == j) ? 1.0f : 0.0f;
-    }
-    for (int i = 0; i < FC2_OUT; i++) {
-        model.fc2.biases[i] = 1.0f;
-        for (int j = 0; j < FC1_OUT; j++)
-            model.fc2.weights[i][j] = (i == j) ? 1.0f : 0.0f;
-    }
-}
 
 void* pipeline_thread(void* arg) {
     Sample* s = (Sample*)arg;
     int id = s->id;
 
-    for (int d = 0; d < CONV_DEPTH; d++)
-        for (int i = 0; i < CONV_OUT; i++)
-            for (int j = 0; j < CONV_OUT; j++) {
-                float sum = model.conv.biases[d];
-                for (int c = 0; c < CHANNELS; c++)
-                    for (int ki = 0; ki < KERNEL_SIZE; ki++)
-                        for (int kj = 0; kj < KERNEL_SIZE; kj++)
-                            sum += model.conv.weights[d][c][ki][kj] * s->input[i+ki][j+kj][c];
-                s->conv_output[i][j][d] = sum;
-            }
-
-    for (int d = 0; d < CONV_DEPTH; d++)
-        for (int i = 0; i < CONV_OUT; i++)
-            for (int j = 0; j < CONV_OUT; j++)
-                s->relu_output[i][j][d] = (s->conv_output[i][j][d] > 0) ? s->conv_output[i][j][d] : 0;
-
-    int idx = 0;
-    for (int d = 0; d < CONV_DEPTH; d++)
-        for (int i = 0; i < CONV_OUT; i += 2)
-            for (int j = 0; j < CONV_OUT; j += 2) {
-                float maxval = s->relu_output[i][j][d];
-                for (int di = 0; di < 2; di++)
-                    for (int dj = 0; dj < 2; dj++) {
-                        float val = s->relu_output[i+di][j+dj][d];
-                        if (val > maxval) maxval = val;
-                    }
-                s->pooled_output[i/2][j/2][d] = maxval;
-                s->flat[idx++] = maxval;
-            }
-
-    for (int i = 0; i < FC1_OUT; i++) {
-        s->fc1_output[i] = model.fc1.biases[i];
-        for (int j = 0; j < (CONV_OUT/2)*(CONV_OUT/2)*CONV_DEPTH; j++)
-            s->fc1_output[i] += model.fc1.weights[i][j] * s->flat[j];
-    }
-
-    for (int i = 0; i < FC2_OUT; i++) {
-        s->fc2_output[i] = model.fc2.biases[i];
-        for (int j = 0; j < FC1_OUT; j++)
-            s->fc2_output[i] += model.fc2.weights[i][j] * s->fc1_output[j];
-    }
+    conv_forward(&model, s->input, s->conv_output);
+    relu_forward(s->conv_output, s->relu_output);
+    maxpool_forward(s->relu_output, s->pooled_output);
+    flatten(s->pooled_output, s->flat);
+    fc1_forward(&model, s->flat, s->fc1_output);
+    fc2_forward(&model, s->fc1_output, s->fc2_output);
 
     pthread_mutex_lock(&print_mutex);
     printf("\n== Thread PID %d, TID %ld processing input %d ==\n", getpid(), gettid(), id);
@@ -149,13 +57,15 @@ int main() {
     gettimeofday(&start, NULL);
 
     pthread_mutex_init(&print_mutex, NULL);
-    initialize_weights();
+    initialize_weights(&model);
     pthread_t threads[NUM_INPUTS];
+
     for (int i = 0; i < NUM_INPUTS; i++) {
         initialize_input(samples[i].input, i);
         samples[i].id = i;
         pthread_create(&threads[i], NULL, pipeline_thread, &samples[i]);
     }
+
     for (int i = 0; i < NUM_INPUTS; i++) pthread_join(threads[i], NULL);
 
     gettimeofday(&end, NULL);
@@ -163,6 +73,7 @@ int main() {
     getrusage(RUSAGE_SELF, &usage);
     double elapsed = (end.tv_sec - start.tv_sec) * 1000.0 +
                      (end.tv_usec - start.tv_usec) / 1000.0;
+
     printf("\n== Performance Metrics ==\n");
     printf("Elapsed time       : %.3f ms\n", elapsed);
     printf("Max RSS memory     : %ld KB\n", usage.ru_maxrss);
@@ -171,3 +82,4 @@ int main() {
 
     return 0;
 }
+
